e8592a9d017ce18e2e0557696e41d5c5935fc73d
927joanna <sot.joanna@gmail.com>
2023-05-16 16:02:29 +0800

    remove comments, json msges

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,13 +4,6 @@ const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
   try {
-    // const auth = await Staff.findOne({ staff_nric: req.body.user });
-    // if (auth) {
-    //   return res
-    //     .status(400)
-    //     .json({ status: "error", msg: "duplicate staff Ic" });
-    // }
-
     const hash = await bcrypt.hash(req.body.password, 12);
 
     await Staff.updateOne(
@@ -18,10 +11,10 @@ const register = async (req, res) => {
     { staff_password: hash }
     );
 
-    res.json({ status: "ok", msg: "user registered" });
+    res.json({ status: "ok", msg: "password updated" });
   } catch (error) {
     console.log(error.message);
-    res.status(400).json({ status: "error", msg: "registration error" });
+    res.status(400).json({ status: "error", msg: "password updated" });
   }
 };
 
@@ -35,7 +28,7 @@ const login = async (req, res) => {
       }
       const result = await bcrypt.compare(req.body.password, userDetails.staff_password);
       if (!result) {
-        return res.status(401).json({ status: "error", message: "login fail 1" });
+        return res.status(401).json({ status: "error", message: "login fail" });
       } else {
         res.json(userDetails)
       }
@@ -47,14 +40,14 @@ const login = async (req, res) => {
   
       const result = await bcrypt.compare(req.body.password, userDetails.contact_password);
       if (!result) {
-        return res.status(401).json({ status: "error", message: "login fail 2" });
+        return res.status(401).json({ status: "error", message: "login fail" });
       } else {
         res.json(userDetails)
       }
     } 
 
   } catch (error) {
-    return res.status(400).json({ status: "error", msg: "login fail 3" });
+    return res.status(400).json({ status: "error", msg: "login fail" });
   }
 };
 
