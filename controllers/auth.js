ef69f5514f33163e3ef862c4d5c96cd9ca27a57e
927joanna <sot.joanna@gmail.com>
2023-05-16 15:56:16 +0800

    register (updates password field only), and login works

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,22 @@
 const Staff = require("../models/Staff");
+const Contacts = require("../models/Contacts");
 const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
   try {
-    const auth = await Staff.findOne({ staff_nric: req.body.user });
-    if (auth) {
-      return res
-        .status(400)
-        .json({ status: "error", msg: "duplicate staff Ic" });
-    }
+    // const auth = await Staff.findOne({ staff_nric: req.body.user });
+    // if (auth) {
+    //   return res
+    //     .status(400)
+    //     .json({ status: "error", msg: "duplicate staff Ic" });
+    // }
 
     const hash = await bcrypt.hash(req.body.password, 12);
 
-    await Staff.create({
-      staff_nric: req.body.staff_nric,
-      hash,
-    });
+    await Staff.updateOne(
+    { staff_nric: req.body.user },
+    { staff_password: hash }
+    );
 
     res.json({ status: "ok", msg: "user registered" });
   } catch (error) {
@@ -27,36 +28,37 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   try {
     if (req.body.role == 'staff') {
+      console.log('a')
       const userDetails = await Staff.findOne({ staff_nric: req.body.user });
-      console.log(bcrypt.hash("password", 12))
-      if (!auth) {
+      if (!userDetails) {
         return res.status(400).json({ status: "error", msg: "not authorised" });
       }
-  
-      const result = await bcrypt.compare(req.body.password, userDetails.password);
+      const result = await bcrypt.compare(req.body.password, userDetails.staff_password);
       if (!result) {
-        return res.status(401).json({ status: "error", message: "login fail" });
+        return res.status(401).json({ status: "error", message: "login fail 1" });
+      } else {
+        res.json(userDetails)
       }
-      res.json(userDetails)
     } else {
       const userDetails = await Contacts.findOne({ contact_patientNric: req.body.user });
       if (!auth) {
         return res.status(400).json({ status: "error", msg: "not authorised" });
       }
   
-      const result = await bcrypt.compare(req.body.password, userDetails.password);
+      const result = await bcrypt.compare(req.body.password, userDetails.contact_password);
       if (!result) {
-        return res.status(401).json({ status: "error", message: "login fail" });
+        return res.status(401).json({ status: "error", message: "login fail 2" });
+      } else {
+        res.json(userDetails)
       }
-      res.json(userDetails)
     } 
 
   } catch (error) {
-    return res.status(400).json({ status: "error", msg: "login failed" });
+    return res.status(400).json({ status: "error", msg: "login fail 3" });
   }
 };
 
 module.exports = {
-  // register,
+  register,
   login,
 };
